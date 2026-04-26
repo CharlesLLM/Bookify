e11df1dc0be9a2b6d61f3cb2282ba0e490828f6f
CharlesLLM <charlesllm@orange.fr>
2026-04-27 00:42:47 +0200

    Use user module
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, PrismaService, UserService],
+  imports: [UserModule],
+  providers: [JwtService],
 })
 export class AppModule {}
