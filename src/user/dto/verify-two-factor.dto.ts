6f19b9a9ce34638bd2bd5e176a3016790caf3134
monkyy <charlie.mass.cm@gmail.com>
2026-04-28 10:30:35 +0200

    added apiproperty 2 verify two factor

--- src/user/dto/verify-two-factor.dto.ts
+++ src/user/dto/verify-two-factor.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumberString } from 'class-validator';
 
 export class VerifyTwoFactorDto {
+  @ApiProperty({
+    description: 'The id of the two-factor row in the db linked to user logging in',
+    example: '123',
+  })
   @IsString()
   @IsNotEmpty()
   twoFactorCodeId!: string;
 
+  @ApiProperty({
+    description: 'The code sent to the user\'s email',
+    example: '123',
+  })
   @IsNumberString()
   @IsNotEmpty()
   code!: string;
